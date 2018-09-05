303779b8e5b5c796df1ee865dd6c8de7ed65f440
blank <bakareemmanuel98@outlook.com>
2018-09-05 12:03:36 +0100

    Implemented login and JWT authentication

--- app.js
+++ app.js
@@ -52,11 +52,13 @@ app.use(cookieParser());
 // Enable CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Access-Token');
   next();
 });
 
 const URL_PREFIX = '/api/v1';
+
+app.use(new RegExp(`${URL_PREFIX}/(?!auth).*`, 'i'), Auth.verifyToken);
 
 app.post(`${URL_PREFIX}/auth/register`, Auth.register)
   .describe({
--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ export default class Auth {
 
     req.body.password = bcrypt.hashSync(req.body.password, 8);
 
-    new UserModel().insert(req.body, ['id', 'username', 'name']).then((user) => {
+    new UserModel().selectOne(['id', 'username', 'name']).then((user) => {
       // create a token
       const token = jwt.sign({ id: user.id }, Config('development').KEY, {
         expiresIn: 86400, // expires in 24 hours
@@ -41,6 +41,37 @@ export default class Auth {
       Auth.getLogger()(`Sending token back to user: ${token}`);
       res.status(200).send({ auth: true, token, user: user.rows[0] });
     })
-      .catch(() => setImmediate(() => res.status(500).send('There was a problem registering the user.')));
+      .catch(() => setImmediate(() => res.status(500).json({ error: 'There was a problem registering the user.' })));
+  }
+
+  static verifyToken(req, res, next) {
+    Auth.getLogger()(`Fetching request for user with the following details: \n
+    Headers: ${JSON.stringify(req.headers)} \n
+    Method: ${req.method}`);
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (req.method === 'OPTIONS') {
+      return res.status(200).json({ success: true });
+    }
+
+    // decode token
+    if (token) {
+      // verifies secret and checks token
+      jwt.verify(token, Config('development').KEY, (err, decoded) => {
+        if (err) {
+          return res.json({ success: false, message: 'Failed to authenticate token.' });
+        }
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      });
+    } else {
+      // if there is no token
+      // return an error
+      res.status(403).json({
+        success: false,
+        message: 'No token provided.',
+      });
+    }
   }
 }
